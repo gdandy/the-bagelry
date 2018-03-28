a8bd1e0f2197d81f2d8c6f000f99c00b6d47143e
gdandy <aabrown@gmail.com>
2018-03-28 09:05:46 -0500

    Updated to mountain theme and new Menu scroll transition
    - Updated to mountain theme.
    - Header image blue-ish overlay.
    - Menu scroll transition.
    - Logo without circle border.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface ImageAlt {
+  src: string;
+  altText: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  images: Array<string>;
+  images: Array<ImageAlt> = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.images = [
-      './assets/img/cs-1/sandwich-breakfast-14er.jpg',
-      './assets/img/cs-1/the-bagelry-sign.jpg',
-      './assets/img/cs-1/drinks-dazbog-coffee.jpg',
-      './assets/img/cs-1/bagel-mountain-power.jpg'
-     ];
+    this.images.push({ src: './assets/img/cs-1/sandwich-breakfast-14er.jpg', altText: 'The 14er Breakfast Sandwich'});
+    this.images.push({ src: './assets/img/cs-1/the-bagelry-sign.jpg', altText: 'The Bagelry' });
+    this.images.push({ src: './assets/img/cs-1/drinks-dazbog-coffee.jpg', altText: 'Proudly serving Dazbog Coffee' });
+    this.images.push({ src: './assets/img/cs-1/bagel-mountain-power.jpg', altText: 'The Mountain Power Bagel' });
   }
 }
